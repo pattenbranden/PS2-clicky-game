ba4b88a0c4262a2f53f413391c69c91d9e9556da
BRANDEN PATTEN <patten.branden@gmail.com>
2020-04-28 04:43:30 -0400

    Updates

---
+++ precache-manifest.a7f72a7a018dc6931cf821dfbc8b9256.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c383ad9eccd9d35b048f388ac56ab99c",
+    "url": "/PS2-clicky-game-react/index.html"
+  },
+  {
+    "revision": "85b8e5240a47bba9b846",
+    "url": "/PS2-clicky-game-react/static/css/main.b4802167.chunk.css"
+  },
+  {
+    "revision": "4323e600acebf6d5a8f4",
+    "url": "/PS2-clicky-game-react/static/js/2.cd356f4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PS2-clicky-game-react/static/js/2.cd356f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b8e5240a47bba9b846",
+    "url": "/PS2-clicky-game-react/static/js/main.e56569a4.chunk.js"
+  },
+  {
+    "revision": "df396aee9815c0ddcb11",
+    "url": "/PS2-clicky-game-react/static/js/runtime-main.96110990.js"
+  },
+  {
+    "revision": "19d7643a2fdc67c73319db68289889ca",
+    "url": "/PS2-clicky-game-react/static/media/ps2-bg.19d7643a.jpg"
+  }
+]);
